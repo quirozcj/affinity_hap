7b69868f50dd4c79dbd88ccbff865be877fe41ea
quirozcj <63603068+quirozcj@users.noreply.github.com>
2022-01-31 11:40:09 +0000

    added sliding windows to affinity

--- run_affinity.sh
+++ run_affinity.sh
@@ -1,49 +1,21 @@
-#!/bin/bash
-#SBATCH --partition=nbi-medium,jic-medium,nbi-long,jic-long,RG-Cristobal-Uauy
-#SBATCH --nodes=1
-#SBATCH --cpus=1
-#SBATCH --mem 100G
-#SBATCH -o /jic/scratch/groups/Cristobal-Uauy/quirozj/09_watseq/03_haplotypes/ibspy_analysis/scripts/log/aff_mt.%N.%j.out # STDOUT
-#SBATCH -e /jic/scratch/groups/Cristobal-Uauy/quirozj/09_watseq/03_haplotypes/ibspy_analysis/scripts/log/aff_mt.%N.%j.err # STDERR
-#SBATCH --job-name=aff_mt
-#SBATCH --array=0-20
 
-i=$SLURM_ARRAY_TASK_ID
+declare -a references=("chinese" "arinalrfor")
+declare -a dampings=(0.5 0.6)
 
-# chromosome list
-declare -a chromosomes=()
-for n in {1..7..1}; do
-	for l in A B D; do
-		chromosomes+=(chr${n}${l});
-	done;
-done
-
-declare -a references=("chinese" "arinalrfor" "sy_mattis" "jagger" "stanley" "julius" "lancer" "landmark" "mace" "norin61" "spelta")
-declare -a assemblies=("arinalrfor")
-
-chr_i=$(($i%21))
-chromosome=${chromosomes[$chr_i]}
-
-ref_i=$(($i/21))
-assembly=${assemblies[$ref_i]}
-
-declare -a dampings=(0.5 0.6 0.7 0.8 0.9 0.91 0.92 0.93 0.94 0.95 0.96 0.97 0.98 0.99)
-# declare -a dampings=(0.5 0.6)
-
-ibs_window=50000
+chromosome='chr1A'
+assembly='chinese'
+ibs_window=100000
 windows_no=19
-score=variations_modern_tauschii
+sliding_w=3
 
-base_dir=/jic/scratch/groups/Cristobal-Uauy/quirozj/09_watseq
-script_dir=$base_dir/03_haplotypes/ibspy_analysis/scripts
-db_dir=$base_dir/03_haplotypes/ibspy_analysis/ibspy_combined/variations_tauschii/references_combined
+test_data='./test_data'
 conversion_dir=$base_dir/09_mummer_conversions/by_chromosome
-out_dir=/nbi/group-data/ifs/NBI/Cristobal-Uauy/Jesus/14_watseq_affinity/affinity_long_files/modern_tauschii/${assembly}/${score}
+out_dir='./out'
 mkdir -p $out_dir
 
-python3 $script_dir/affinity_by_windows.py \
--c $conversion_dir/${chromosome}_merged_in_windows__ws-100000_round-3_flank-50000.tsv.gz \
--p $db_dir \
+python3 affinity_by_windows.py \
+-c $test_data/${chromosome}_merged_in_windows__ws-100000_round-3_flank-50000.tsv.gz \
+-p $test_data \
 -k $chromosome \
 -r ${references[@]} \
 -a $assembly \
@@ -51,5 +23,6 @@ python3 $script_dir/affinity_by_windows.py \
 -g ${dampings[@]} \
 -w $ibs_window \
 -l chr_lenghts.tsv \
--d modern_tauschii_HC.tsv \
--o $out_dir/${chromosome}_${assembly}_${windows_no}w_${ibs_window}ibs.tsv.gz
+-d ${dataset}_HC.tsv \
+-s ${sliding_w} \
+-o $out_dir/${chromosome}_${assembly}_${windows_no}w_${ibs_window}ibs_sliding${sliding_w}.tsv.gz
